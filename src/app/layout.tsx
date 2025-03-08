8c849b920af405e6d10ca8acc929f20dd79987e6
Andrea <ravasi.andrea@live.com>
2025-03-08 13:29:43 +0100

    removed clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { type Metadata } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
 import { Navbar } from "./_components/layout/navbar";
 import Footer from "./_components/layout/footer";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Riddle",
@@ -18,16 +17,14 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <body className="antialiased max-h-screen  max-w-xl mx-4 mt-8 lg:mx-auto bg-black text-white">
-          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 ">
-            <Navbar></Navbar>
-            <TRPCReactProvider>{children}</TRPCReactProvider>
-            <Footer></Footer>
-          </main>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body className="antialiased max-h-screen  max-w-xl mx-4 mt-8 lg:mx-auto bg-black text-white">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 ">
+          <Navbar></Navbar>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+          <Footer></Footer>
+        </main>
+      </body>
+    </html>
   );
 }
